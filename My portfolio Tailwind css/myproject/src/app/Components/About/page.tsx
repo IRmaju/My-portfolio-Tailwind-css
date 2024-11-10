7b1c458631f89ad7699267ebf21c48c1e1a4a3aa
WAJEEHA QADIR ABBASI <160304483+IRmaju@users.noreply.github.com>
2024-11-10 22:51:28 +0500

    Add files via upload

---
+++ My portfolio Tailwind css/myproject/src/app/Components/About/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+    <div>
+       i am a brillian  student of web developer .i can your work with dead line.
+    </div>
+    );
+  }
---
+++ My portfolio Tailwind css/myproject/src/app/Components/Contactus/page.tsx
@@ -0,0 +1,4 @@
+export default function ContactUs() {
+    return <div> you can contact me with this portfolio .
+         give me atleat one chane i will never disppoint you</div>;
+  }
